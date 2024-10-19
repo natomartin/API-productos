50efa74063be07606c1da06da99324f897fe32fd
natomartin <159218598+natomartin@users.noreply.github.com>
2024-10-18 20:08:52 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,73 @@
+import express from 'express';
+import fs, { read, writeFileSync } from "fs";
+import bodyParser from "body-parser";
+
+const app = express()
+app.use(bodyParser.json())
+
+const readData= () =>{
+    try{
+        const data = fs.readFileSync("./db.json");
+        return (JSON.parse(data))
+    }catch(error){
+        console.log(error)
+    }
+};
+const writeData = (data) =>{
+    try {
+        fs.writeFileSync("./db.json", JSON.stringify(data));
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+app.get("/productos/list",(req, res)=>{
+    const data=readData();
+    res.json(data.productos)
+});
+
+app.get("/productos/searchById/:id",(req, res)=>{
+    const data=readData();
+    const id=parseInt(req.params.id);
+    const producto = data.productos.find((producto) => producto.id===id);
+    res.json(producto);
+});
+
+app.post("/productos/new",(req, res)=>{
+    const data=readData();
+    const body=req.body;
+    const newProducto={
+        id: data.productos.length+1,
+        ...body,
+    };
+    data.productos.push(newProducto);
+    writeData(data);
+    res.json(newProducto);
+});
+
+app.put("/productos/update/:id",(req, res)=>{
+    const data=readData();
+    const body=req.body;
+    const id=parseInt(req.params.id);
+    const productoIndex=data.productos.findIndex((producto)=>producto.id===id);
+    data.productos[productoIndex]={
+        ...data.productos[productoIndex],
+        ...body,
+    };
+    writeData(data);
+    res.json({message:"Si"});
+});
+
+app.delete("/productos/delete/:id", (req,res)=>{
+    const data=readData();
+    const id=parseInt(req.params.id);
+    const productoIndex=data.productos.findIndex((producto)=>producto.id===id);
+    data.productos.splice(productoIndex, 1);
+    writeData(data);
+    res.json({message:"Si"})
+});
+
+app.listen(3000, ()=> {
+    console.log('Server listening on port 3000')
+})
+
